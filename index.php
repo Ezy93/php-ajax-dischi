b17b9c8f6a1f1380ce1d7fd8875c4ce9345a8dbb
Ezy93 <giordani001@gmail.com>
2022-04-22 14:59:09 +0200

    compleata prima milestone con la stampa a schermo degli albums tramite l'utilizzo di solo php

--- index.php
+++ index.php
@@ -18,13 +18,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- import di vuejs -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <title>php-ajax-dischi</title>
 </head>
 <body>
+    <?php include __DIR__.'/partials/header.php' ?>
+    <?php include __DIR__.'/partials/main.php' ?>
+
     <!-- importato file js -->
     <script src="js/script.js"></script>
-    <!-- import di vuejs -->
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
     <!-- import di axios -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.26.1/axios.min.js" integrity="sha512-bPh3uwgU5qEMipS/VOmRqynnMXGGSRv+72H/N260MQeXZIK4PG48401Bsby9Nq5P5fz7hy5UGNmC/W1Z51h2GQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
--- partials/main.php
+++ partials/main.php
@@ -1,3 +1,22 @@
+<?php 
+    include __DIR__.'/../server/data.php'
+?>
+
 <main>
-    
+    <?php foreach($albums as $album){ ?>
+        <ul>
+            <li>
+                <img src="<?= $album['album_cover']  ?>" alt="<?= $album['title']  ?>">
+            </li>
+            <li>
+                <?= $album['title']  ?>
+            </li>
+            <li>
+                <?= $album['author']  ?>
+            </li>
+            <li>
+            <?= $album['year']  ?>
+            </li>
+        </ul>
+    <?php } ?>
 </main>
