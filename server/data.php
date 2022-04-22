105d9f209eb943eb8ad61b7c842403a2413669dc
Ezy93 <giordani001@gmail.com>
2022-04-22 14:37:06 +0200

    inizializzati data per la creazione successiva della api

---
+++ server/data.php
@@ -0,0 +1,75 @@
+<?php 
+    $albums=[
+        [
+            'title' => 'new jersey',
+            'author' => 'Bon Jovi',
+            'genre' => 'Rock',
+            'year' => 1988,
+            'album_cover' => 'https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg',
+        ],
+        [
+            'title' => 'live at wembley 86',
+            'author' => 'Queen',
+            'genre' => 'Pop',
+            'year' => 1992,
+            'album_cover' => 'https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg',
+        ],
+        [
+            'title' => 'ten\'s summoner\'s tales',
+            'author' => 'Sting',
+            'genre' => 'Pop',
+            'year' => 1993,
+            'album_cover' => 'https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg',
+        ],
+        [
+            'title' => 'steve gadd band',
+            'author' => 'Steve Gadd Band',
+            'genre' => 'Jazz',
+            'year' => 2018,
+            'album_cover' => 'https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg',
+        ],
+        [
+            'title' => 'brave new world',
+            'author' => 'Iron Maiden',
+            'genre' => 'Metal',
+            'year' => 2000,
+            'album_cover' => 'https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg',
+        ],
+        [
+            'title' => 'one more car, one more rider',
+            'author' => 'Eric Clapton ',
+            'genre' => 'Rock',
+            'year' => 2002,
+            'album_cover' => 'https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg',
+        ],
+        [
+            'title' => 'made in japan',
+            'author' => 'Deep Purple',
+            'genre' => 'Rock',
+            'year' => 1972,
+            'album_cover' => 'https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg',
+        ],
+        [
+            'title' => 'and justice for all',
+            'author' => 'Metallica',
+            'genre' => 'Metal',
+            'year' => 1988,
+            'album_cover' => 'https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg',
+        ],
+        [
+            'title' => 'hard wired',
+            'author' => 'Dave Weckl',
+            'genre' => 'Jazz',
+            'year' => 1994,
+            'album_cover' => 'https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg',
+        ],
+        [
+            'title' => 'bad',
+            'author' => 'Michael Jackson',
+            'genre' => 'Pop',
+            'year' => 1987,
+            'album_cover' => 'https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg',
+        ],
+    ]
+
+?>
