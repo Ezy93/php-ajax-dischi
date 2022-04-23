8037dee0e75983a958179394753bcf7b30b9a666
Ezy93 <giordani001@gmail.com>
2022-04-23 15:42:33 +0200

    completata stilizzazione grafica delle card degli album

--- partials/main.php
+++ partials/main.php
@@ -1,20 +1,15 @@
 
 
-<main id="app">
-    
-        <ul v-for="album in albums">
-            <li>
-                <img :src="album.album_cover" :alt="album.title">
-            </li>
-            <li>
-                {{album.title}}
-            </li>
-            <li>
-                {{album.author}}
-            </li>
-            <li>
-                {{album.year }}
-            </li>
-        </ul>
+<main id="app" >
+    <div class="my-container d-flex flex-wrap mt-3">
+        <div class="card flex-shrink-0 m-2" v-for="album in albums" >
+            <img :src="album.album_cover" class="card-img-top p-3" :alt="album.title">
+            <div class="card-body text-center pb-1">
+                <h4 class="card-title text-white mb-4 fw-bold text-uppercase">{{album.title}}</h4>
+                <p class="card-text my-text-card mb-0">{{album.author}}</p>
+                <p class="card-text my-text-card">{{album.year }}</p>
+            </div>
+        </div>
+    </div>
     
 </main>
