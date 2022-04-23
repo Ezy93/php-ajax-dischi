152a86512f7a7f1859864001ad211fa50ffc6a7f
Ezy93 <giordani001@gmail.com>
2022-04-23 15:11:10 +0200

    aggiunto header con logo spotify

--- index.php
+++ index.php
@@ -20,6 +20,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- import di vuejs -->
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <!-- import cdn bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- import foglio di stile custom -->
+    <link rel="stylesheet" href="style/style.css">
     <title>php-ajax-dischi</title>
 </head>
 <body>
---
+++ partials/header.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="my-logo d-flex align-items-center">
+        <img class="img-fluid" src= "img/spotify-logo.png" alt="logo spotify">
+    </div>
+</header>
